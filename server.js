fdf1aa2262a88e9c941ea34d375a4a135b97d80c
Matthew Gotteiner <matthew.gotteiner@gmail.com>
2015-03-07 00:25:13 -0500

    server can write / read from db, receive http requests

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var cheerio = require('cheerio'), http = require('http');
+var cheerio = require('cheerio'), http = require('http'), MongoClient = require('mongodb').MongoClient, assert = require('assert'), url = require('url');
 
 function getSoundPage(pageNum, callback) {
 	var options = {
@@ -7,29 +7,54 @@ function getSoundPage(pageNum, callback) {
 		port: 80
 	};
 	http.get(options, function(res) {
+		assert.equal(res.statusCode, 200);
 		var body = '';
 		res.on('data', function(p) {
 			body += p;
 		});
 		res.on('end', function() {
 			$ = cheerio.load(body);
-			pageSounds = [];
+			sounds = [];
 			$('.instant').each(function() {
 				var name = $(this).text().trim();
 				var url = $(this).find('.small-button').attr('onclick').match(/\'.*\'/)[0];
 				url = url.substring(1, url.length - 1)
 				url = "www.myinstants.com" + url;
-				pageSounds.push({'name': name, 'url': url});
+				sounds.push({'name': name, 'url': url});
 			});
-			callback.gotSounds(pageSounds);
+			callback(pageNum, sounds);
 		});
 	}).on('error', function(e) {
-		callback.error(e);
+		assert.fail(e, null);
 	});
 }
 
+MongoClient.connect('mongodb://localhost:27017/sounds', function(err, db) {
+		assert.equal(err, null);
+		for(var i = 1; i <= 5; i++) {
+			getSoundPage(i, function(pageNum, sounds) {
+				var collection = db.collection('page-' + pageNum);
+				collection.drop(function(err) {
+					assert.equal(err, null);
+					collection.insert(sounds, function(err, result) {
+						assert.equal(err, null);
+					});
+				});
+			});
+		}
+		http.createServer(function(req, res) {
+			var query = url.parse(req.url, true).query;
+			if(query.page) {
+				var collection = db.collection('page-' + query.page);
+				collection.find({}, {_id: false}).toArray(function(err, items) {
+					assert.equal(null, err);
+					res.writeHead(200, {'Content-Type': 'application/json'});
+					res.end(JSON.stringify(items));
+				});
 
-getSoundPage(1, {
-	gotSounds: function(sounds) { console.log("got sound: ", sounds); },
-	error: function(e) { console.log("error", e); }
+			} else {
+				res.writeHead(400, {'Content-Type': 'application/json'});
+				res.end('{error: \'bad page number\'}');
+			}
+		}).listen(1337, '127.0.0.1');
 });
