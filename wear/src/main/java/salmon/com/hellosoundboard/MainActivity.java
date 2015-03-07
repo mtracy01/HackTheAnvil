231b7d7370a1f614e20d4da852a97fa4e03fde7b
Tim Vincent <vincent3@purdue.edu>
2015-03-07 14:39:04 -0500

    Added android wear button animations

--- wear/src/main/java/salmon/com/hellosoundboard/MainActivity.java
+++ wear/src/main/java/salmon/com/hellosoundboard/MainActivity.java
@@ -3,11 +3,18 @@ package salmon.com.hellosoundboard;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+    private static final String TAG = "Wear Main Act";
     private TextView mTextView;
+    boolean down = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +25,29 @@ public class MainActivity extends Activity {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
                 mTextView = (TextView) stub.findViewById(R.id.text);
+                final ImageButton button = (ImageButton) findViewById(R.id.imageButton);
+                button.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View view, MotionEvent event) {
+                        String s = "";
+                        s += "action=" + event.getAction();
+                        s += ", X=" + event.getX();
+                        s += ", Y=" + event.getY();
+                        Log.d(TAG, s);
+                        if(!down){
+                            button.setImageResource(R.drawable.btn_red_down);
+                            down = true;
+                        }
+                        else{
+                            button.setImageResource(R.drawable.btn_red_up);
+                            down = false;
+                        }
+                        return true;
+                    }
+                });
             }
         });
+
     }
+
 }
